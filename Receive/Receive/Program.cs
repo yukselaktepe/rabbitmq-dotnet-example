e6083ca3f829847bdab1c504886694408784f661
Yüksel Aktepe <yukselaktepe@hotmail.com.tr>
2019-05-17 07:42:35 +0300

    Initial commit

---
+++ Receive/Receive/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+
+                channel.QueueDeclare(queue: "Example",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($" message : " + message);
+                };
+
+                channel.BasicConsume(queue: "Example",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Sender/Sender/Program.cs
@@ -0,0 +1,78 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine("> value<enter>");
+                Console.WriteLine("Ctrl-C to quit.\n");
+
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cancelled = true;
+                };
+
+                while (!cancelled)
+                {
+                    Console.Write("> ");
+
+                    string text;
+                    try
+                    {
+                        text = Console.ReadLine();
+                    }
+                    catch (IOException)
+                    {
+                        break;
+                    }
+                    if (text == null)
+                    {
+                        break;
+                    }
+
+                    try
+                    {
+                        channel.QueueDeclare(queue: "Example",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                        var body = Encoding.UTF8.GetBytes(text);
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "Example",
+                                             basicProperties: null,
+                                             mandatory: false,
+                                             body: body);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"failed to deliver message: {e.Message}");
+                    }
+                }
+
+               
+
+
+            }
+
+            Console.WriteLine(" İlgili kişi gönderildi...");
+        }
+    }
+}
